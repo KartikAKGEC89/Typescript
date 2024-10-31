c7b11ef15d635c416a5ccf5b49506a13856fa4a7
KartikAKGEC89 <s11kartik2111087@gmail.com>
2024-10-31 12:07:25 +0530

    Add CSS into file

--- React-TypeScript/myapp/src/App.tsx
+++ React-TypeScript/myapp/src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 // import Greet from './components/Greet';
 // import Status from './components/Status';
 // import Heading from './components/Heading';
-import Button from './components/Button';
+// import Button from './components/Button';
+import Container from './components/Container';
 
 function App() {
   
@@ -11,9 +12,10 @@ function App() {
       {/* <Greet name="Kartik" num={10} /> */}
       {/* <Status status="Loading" /> */}
       {/* <Heading>Heading is here....</Heading> */}
-      <Button handleClick={(event) => {
-        console.log("Handle Click function")
-      }} />
+      {/* <Button handleClick={(event, id) => {
+        console.log("Handle Click function", event, id)
+      }} /> */}
+      <Container />
     </div>
   );
 }
---
+++ React-TypeScript/myapp/src/components/Container.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Container = () => {
+  return (
+    <div>
+      <h1 style={{border:"solid black 1px", padding:"1rem"}}> Hello </h1>
+    </div>
+  )
+}
+
+export default Container
